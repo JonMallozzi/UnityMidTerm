6120712d46747be5c3820f45eada054f2f3406b1
yoctomentric <willcunningham64@gmail.com>
2020-02-27 18:42:58 -0500

    Added turret and bullet. Extremely basic targeting system, and bullets cannot do damage as there is nothing to deal damage to.

---
+++ Sword Slashing Game/Assets/Will's things/Prefabs/Turret/BulletController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 1;
+    public int damage = 1;
+    private void FixedUpdate()
+    {
+        //move along the y
+        transform.position += transform.up * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Contains("Player"))//placeholder for until Austin makes a character controller
+        {
+            //other.GetComponent<Health>().TakeDamage(damage);
+            Remove();
+        }
+    }
+
+    private void Remove()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Sword Slashing Game/Assets/Will's things/Prefabs/Turret/Turret.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField] Transform rotator;
+    [SerializeField] Transform firePoint;
+
+    [SerializeField] float coolDown = 1;
+    private float currentFireTime = 1;
+    [SerializeField] float fireForce = 500;
+    [SerializeField] int damage = 1;
+
+
+    [SerializeField] BulletController bullet;
+
+    void Start()
+    {
+        if(target == null)
+        {
+            //target = GameObject.FindObjectOfType<PlayerController>().transform
+        }
+    }
+
+    void Update()
+    {
+        //aim
+        rotator.LookAt(target);
+        //shooting
+
+        if(currentFireTime >= 0)
+        {
+            currentFireTime -= Time.deltaTime;
+        }
+        else
+        {
+            currentFireTime = coolDown;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        print("shoot " + Time.time);
+        BulletController bull = Instantiate(bullet, firePoint.position, firePoint.rotation);
+        bull.speed = fireForce;
+        bull.damage = damage;
+    }
+}
