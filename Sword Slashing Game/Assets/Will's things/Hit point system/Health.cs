ccc13bae8902fc2e9ac4c8a811a0e3092bcdfb6c
yoctomentric <willcunningham64@gmail.com>
2020-03-03 18:06:27 -0500

    Health system added.

---
+++ Sword Slashing Game/Assets/Will's things/Hit point system/Health.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public UnityEvent deathEvent;
+    public UnityEvent damageEvent;
+    public UnityEvent healEvent;
+
+    public int maxHealth = 10;
+    public int currentHealth = 10;
+    bool dead = false;
+
+    public void ModifyHealth(int amount)
+    {
+        if (dead)
+        {
+            return; //skip if you die
+        }
+        //run events
+        if(amount > 0)
+        {
+            healEvent?.Invoke();
+        }
+        else if (amount < 0)
+        {
+            damageEvent?.Invoke();
+        }
+        //add or subtract health
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        if(currentHealth == 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //remove character
+        dead = true;
+        if(deathEvent != null)
+        {
+            deathEvent?.Invoke();
+        }
+        else
+        {
+            print("ERROR NO DEATH EFFECT");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            ModifyHealth(-1);
+        }
+    }
+
+    void Ping()
+    {
+        Debug.Log("Ping");
+    }
+}
