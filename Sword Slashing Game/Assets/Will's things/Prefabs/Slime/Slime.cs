00bf5ef179b216bf498df15753cf28e9e56d059b
yoctomentric <willcunningham64@gmail.com>
2020-03-05 18:22:36 -0500

    Added slime enemy prototype. Still needs 3d model and the ability to deal damage or die

---
+++ Sword Slashing Game/Assets/Will's things/Prefabs/Slime/Slime.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Slime : MonoBehaviour
+{
+    public Transform target;
+    Transform looker;
+    [SerializeField] LayerMask groundMask;
+    Rigidbody rb;
+    Collider col;
+
+    public Vector2 jumpForceVectors;
+    bool grounded = false;
+
+    private void Start()
+    {
+        col = gameObject.GetComponent<Collider>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        looker = new GameObject().transform;
+        looker.SetParent(this.transform);
+        ///if(!target){
+        ///     target = GameObject.FindObjectOfType<PlayerController>();
+        ///}
+        ///       
+        StartCoroutine(MovementLoop());
+
+    }
+    void Jump()
+    {
+        //look at
+        looker.LookAt(target);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, looker.eulerAngles.y, transform.eulerAngles.z);
+        //slime is now looking at player. Add force to forward and upward axis
+        rb.AddForce(transform.forward * jumpForceVectors.x);
+        rb.AddForce(transform.up * jumpForceVectors.y);
+
+    }
+
+    IEnumerator MovementLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            grounded = Physics.Raycast(transform.position, Vector3.down, col.bounds.size.y + 0.01f, groundMask);
+            if (grounded)
+            {
+                Jump();
+            }
+            else
+            {
+                while (!grounded)
+                {
+                    yield return new WaitForEndOfFrame();
+                    grounded = Physics.Raycast(transform.position, Vector3.down, col.bounds.size.y + 0.01f, groundMask);
+                }
+            }
+        }
+    }
+}
