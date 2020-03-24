4c7398a923416c127e498749f652a639ef0bcff9
Austin Krakowiak <austin.krakowiak@maine.edu>
2020-03-23 23:54:27 -0400

    Added Full Movement Functionality & Jumping

---
+++ Sword Slashing Game/Assets/Austin's Scripts/PlayerMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController cController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        cController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+        JumpInput();
+    }
+
+    private void PlayerMovement()
+    {
+        float hInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vInput;
+        Vector3 rightMovement = transform.right * hInput;
+
+        cController.SimpleMove(forwardMovement + rightMovement);
+    }
+
+    private void JumpInput() 
+    {
+        if(Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        } 
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        cController.slopeLimit = 90.0f;
+        float airTime = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(airTime);
+            cController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            airTime += Time.deltaTime;
+            yield return null;
+        } while (!cController.isGrounded && cController.collisionFlags != CollisionFlags.Above);
+
+        cController.slopeLimit = 45.0f;
+        isJumping = false;
+        
+    }
+}
