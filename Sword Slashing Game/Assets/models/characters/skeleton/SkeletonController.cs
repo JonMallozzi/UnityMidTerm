79df53542dddfce90dacd3e5c6a71b67c83545de
JonMallozzi <jonathan.mallozzi@maine.edu>
2020-03-25 01:38:01 -0400

    made the skeleton follow the player and added the GameManager and Interactable Class

---
+++ Sword Slashing Game/Assets/models/characters/skeleton/SkeletonController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SkeletonController : MonoBehaviour {
+
+   private float speed = 4f;
+   private float rotSpeed = 80f;
+   private float rot = 0f;
+   private float gravity = 8f;
+
+   private Vector3 moveDirection = Vector3.zero;
+
+   private CharacterController controller;
+
+   
+
+   private Animator anim;
+   // Start is called before the first frame update
+    void Start() {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+    
+    // Update is called once per frame
+    void Update() {
+
+        
+        
+        //if (controller.isGrounded) {
+            
+
+			
+        //}    
+    }
+
+}
---
+++ Sword Slashing Game/Assets/scripts/Interactable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//a class for interacting with objects
+public class Interactable : MonoBehaviour
+{
+	public float radius = 3f;
+	
+	bool isFocus = false;
+	Transform player;
+
+	bool hasInteracted = false;
+
+	public virtual void Interact () {}
+
+	void Update () { 
+
+		if (isFocus && !hasInteracted) {
+			float distance = Vector3.Distance(player.position, transform.position);
+			
+			if (distance <= radius){
+				Interact();
+				hasInteracted = true;
+			}
+		}
+	}
+
+	public void onFocused (Transform playerTransform){
+		isFocus = true;
+		player = playerTransform;
+	}
+
+	public void onDefocused (){
+		isFocus = false;
+		player = null;
+	}
+
+	void OnDrawGizmosSelected () {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+}
